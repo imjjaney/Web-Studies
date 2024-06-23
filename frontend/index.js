f8271b2d805e7d04497c7fcccce32c30597e92c1
imjjaney <hmyeonge@naver.com>
2024-06-23 16:02:51 +0900

    Feature: server.js 를 모듈화

---
+++ frontend/index.js
@@ -0,0 +1,4 @@
+let server = require('./server'); // server.js 를 모듈처럼 쓸 예정
+
+server.start(); // 을 했을 때만 서버가 실행되게 하자
+
--- frontend/server.js
+++ frontend/server.js
@@ -3,14 +3,21 @@ let http = require('http');
 // 이를 부르는 게 require 이라는 함수
 // http 모듈을 http 변수 상자에 넣어두고 쓰자.
 
-function onRequest(request, response){
-  response.writeHead(200, {'Content-Type' : 'text/html'}); 
-  response.write('Hello, Node.js');
-  response.end(); 
+
+function start(){
+
+  function onRequest(request, response){
+    response.writeHead(200, {'Content-Type' : 'text/html'}); 
+    response.write('Hello, Node.js');
+    response.end(); 
+  }
+  
+  http.createServer(onRequest).listen(8888); 
+  /**
+   * 서버는 들을 수 있다
+   * 서버가 들을 수 있는 주파수가 8888 (어떤 클라이언트의 이야기를 들을지)
+   */
+
 }
 
-http.createServer(onRequest).listen(8888); 
-/**
- * 서버는 들을 수 있다
- * 서버가 들을 수 있는 주파수가 8888 (어떤 클라이언트의 이야기를 들을지)
- */
+exports.start = start; 
