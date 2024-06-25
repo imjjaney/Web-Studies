11206e0f20ddd39560776301516dbb4a25e97600
imjjaney <hmyeonge@naver.com>
2024-06-25 14:01:09 +0900

    index.js, router, server.js 간 역할 분리

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 let server = require('./server'); // server.js 를 모듈처럼 쓸 예정
+let router = require('./router')
 
-server.start(); // 을 했을 때만 서버가 실행되게 하자
+server.start(router.route); // 을 했을 때만 서버가 실행되게 하자
 
